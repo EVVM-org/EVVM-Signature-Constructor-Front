63c20d038727ac19c8d1bca3ccef7853c7a9af6e
jistro <contact@jistro.xyz>
2025-07-16 17:20:39 -0600

    feat: add simulateMakeOffer function for transaction handling

--- src/utils/TransactionVerify/useMNSTransactionVerify.tsx
+++ src/utils/TransactionVerify/useMNSTransactionVerify.tsx
@@ -2,6 +2,7 @@ import { simulateContract } from "@wagmi/core";
 import { config } from "@/config";
 import MateNameService from "@/constants/abi/MateNameService.json";
 import {
+  MakeOfferInputData,
   PreRegistrationUsernameInputData,
   RegistrationUsernameInputData,
 } from "../TypeInputStructures/mnsTypeInputStructure";
@@ -69,4 +70,41 @@ const verifyRegistrationUsername = async (
     });
 };
 
-export { verifyPreRegistrationUsername, verifyRegistrationUsername };
+const simulateMakeOffer = async (
+  InputData: MakeOfferInputData,
+  mnsAddress: `0x${string}`
+) => {
+  if (!InputData) {
+    return Promise.reject("No data to execute payment");
+  }
+
+  simulateContract(config, {
+    abi: MateNameService.abi,
+    address: mnsAddress,
+    functionName: "makeOffer",
+    args: [
+      InputData.user,
+      InputData.nonce,
+      InputData.username,
+      InputData.amount,
+      InputData.expireDate,
+      InputData.priorityFeeForFisher,
+      InputData.signature,
+      InputData.nonce_Evvm,
+      InputData.priority_Evvm,
+      InputData.signature_Evvm,
+    ],
+  })
+    .then(() => {
+      return Promise.resolve();
+    })
+    .catch((error) => {
+      return Promise.reject(error);
+    });
+};
+
+export {
+  verifyPreRegistrationUsername,
+  verifyRegistrationUsername,
+  simulateMakeOffer,
+};

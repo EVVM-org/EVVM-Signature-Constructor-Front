bd4632faadf6d30cd105e8ec0d899517f6e24a46
jistro <contact@jistro.xyz>
2025-07-20 18:37:18 -0600

    feat: enhance FlushCustomMetadataComponent to fetch price and streamline signature process

--- src/components/SigConstructors/MNSFunctions/FlushCustomMetadataComponent.tsx
+++ src/components/SigConstructors/MNSFunctions/FlushCustomMetadataComponent.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { getAccount } from "@wagmi/core";
+import { getAccount, readContract } from "@wagmi/core";
 import { config } from "@/config/index";
 import { useMnsSignatureBuilder } from "@/utils/SignatureBuilder/useMnsSignatureBuilder";
 import { TitleAndLink } from "@/components/SigConstructors/InputsAndModules/TitleAndLink";
@@ -10,62 +10,114 @@ import { PrioritySelector } from "../InputsAndModules/PrioritySelector";
 import { NumberInputField } from "../InputsAndModules/NumberInputField";
 import { TextInputField } from "../InputsAndModules/TextInputField";
 import { DataDisplayWithClear } from "@/components/SigConstructors/InputsAndModules/DataDisplayWithClear";
+import {
+  FlushCustomMetadataInputData,
+  PayInputData,
+} from "@/utils/TypeInputStructures";
+import { getAccountWithRetry } from "@/utils/getAccountWithRetry";
+import MateNameService from "@/constants/abi/MateNameService.json";
+import { contractAddress, tokenAddress } from "@/constants/address";
+import { executeFlushCustomMetadata } from "@/utils/TransactionExecuter";
 
-
-type FlushCustomMetadataData = {
-  user: `0x${string}`;
-  nonce: string;
-  identity: string;
-  priorityFeeForFisher: string;
-  signature: string;
-  nonce_Evvm: string;
-  priority_Evvm: string;
-  signature_Evvm: string;
+type InfoData = {
+  PayInputData: PayInputData;
+  FlushCustomMetadataInputData: FlushCustomMetadataInputData;
 };
 
 export const FlushCustomMetadataComponent = () => {
   const { signFlushCustomMetadata } = useMnsSignatureBuilder();
   const account = getAccount(config);
   const [priority, setPriority] = React.useState("low");
-  const [dataToGet, setDataToGet] = React.useState<FlushCustomMetadataData | null>(
-    null
-  );
+  const [dataToGet, setDataToGet] = React.useState<InfoData | null>(null);
+
+  const getValue = (id: string) =>
+    (document.getElementById(id) as HTMLInputElement).value;
 
   const makeSig = async () => {
-    const getValue = (id: string) =>
-      (document.getElementById(id) as HTMLInputElement).value;
+    const walletData = await getAccountWithRetry(config);
+    if (!walletData) return;
 
-    const addressMNS = getValue("mnsAddressInput_flushCustomMetadata");
-    const nonceMNS = getValue("nonceMNSInput_flushCustomMetadata");
-    const identity = getValue("identityInput_flushCustomMetadata");
-    const priceToFlushCustomMetadata = getValue("priceForFlushInput_flushCustomMetadata");
-    const priorityFeeForFisher = getValue("priorityFeeInput_flushCustomMetadata");
-    const nonceEVVM = getValue("nonceEVVMInput_flushCustomMetadata");
-    const priorityFlag = priority === "high";
+    const formData = {
+      addressMNS: getValue("mnsAddressInput_flushCustomMetadata"),
+      nonceMNS: getValue("nonceMNSInput_flushCustomMetadata"),
+      identity: getValue("identityInput_flushCustomMetadata"),
+      priorityFeeForFisher: getValue("priorityFeeInput_flushCustomMetadata"),
+      nonceEVVM: getValue("nonceEVVMInput_flushCustomMetadata"),
+      priorityFlag: priority === "high",
+    };
 
-    signFlushCustomMetadata(
-      addressMNS,
-      BigInt(nonceMNS),
-      identity,
-      BigInt(priceToFlushCustomMetadata),
-      BigInt(priorityFeeForFisher),
-      BigInt(nonceEVVM),
-      priorityFlag,
-      (paySignature, flushCustomMetadataSignature) => {
-        setDataToGet({
-          user: (account.address || "0x0") as `0x${string}`,
-          nonce: nonceMNS,
-          identity: identity,
-          priorityFeeForFisher: priorityFeeForFisher,
-          signature: paySignature,
-          nonce_Evvm: nonceEVVM,
-          priority_Evvm: priorityFlag.toString(),
-          signature_Evvm: flushCustomMetadataSignature,
-        });
-      },
-      (error) => console.error("Error signing payment:", error)
-    );
+    readContract(config, {
+      abi: MateNameService.abi,
+      address: formData.addressMNS as `0x${string}`,
+      functionName: "getPriceToFlushCustomMetadata",
+      args: [],
+    })
+      .then((price) => {
+        if (!price) {
+          console.error("Price to flush custom metadata is not available");
+          return;
+        }
+
+        signFlushCustomMetadata(
+          formData.addressMNS,
+          BigInt(formData.nonceMNS),
+          formData.identity,
+          price as bigint,
+          BigInt(formData.priorityFeeForFisher),
+          BigInt(formData.nonceEVVM),
+          formData.priorityFlag,
+          (paySignature, flushCustomMetadataSignature) => {
+            setDataToGet({
+              PayInputData: {
+                from: walletData.address as `0x${string}`,
+                to_address: formData.addressMNS as `0x${string}`,
+                to_identity: "",
+                token: tokenAddress.mate as `0x${string}`,
+                amount: price as bigint,
+                priorityFee: BigInt(formData.priorityFeeForFisher),
+                nonce: BigInt(formData.nonceEVVM),
+                priority: priority === "high",
+                executor: formData.addressMNS as `0x${string}`,
+                signature: paySignature,
+              },
+              FlushCustomMetadataInputData: {
+                user: formData.addressMNS as `0x${string}`,
+                nonce: BigInt(formData.nonceMNS),
+                identity: formData.identity,
+                priorityFeeForFisher: BigInt(formData.priorityFeeForFisher),
+                signature: flushCustomMetadataSignature,
+                nonce_Evvm: BigInt(formData.nonceEVVM),
+                priority_Evvm: formData.priorityFlag,
+                signature_Evvm: paySignature,
+              },
+            });
+          },
+          (error) => console.error("Error signing payment:", error)
+        );
+      })
+      .catch((error) => {
+        console.error("Error fetching price:", error);
+      });
   };
+
+  const execute = async () => {
+      if (!dataToGet) {
+        console.error("No data to execute payment");
+        return;
+      }
+      const mnsAddress = dataToGet.PayInputData.to_address;
+
+      executeFlushCustomMetadata(
+        dataToGet.FlushCustomMetadataInputData,
+        mnsAddress
+      )
+        .then(() => {
+          console.log("Flush custom metadata executed successfully");
+        })
+        .catch((error) => {
+          console.error("Error executing flush custom metadata:", error);
+        });
+    };
 
   return (
     <div className="flex flex-1 flex-col justify-center items-center">
@@ -80,7 +132,13 @@ export const FlushCustomMetadataComponent = () => {
         label="MNS Address"
         inputId="mnsAddressInput_flushCustomMetadata"
         placeholder="Enter MNS address"
+        defaultValue={
+          contractAddress[account.chain?.id as keyof typeof contractAddress]
+            ?.mns || ""
+        }
       />
+
+      <br />
 
       {/* Nonce section with automatic generator */}
 
@@ -131,6 +189,7 @@ export const FlushCustomMetadataComponent = () => {
       <DataDisplayWithClear
         dataToGet={dataToGet}
         onClear={() => setDataToGet(null)}
+        execute={execute}
       />
     </div>
   );

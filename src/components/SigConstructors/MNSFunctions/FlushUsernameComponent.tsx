7fb43ef1a8645558ea9c7a5b5a6103370aa1011e
jistro <contact@jistro.xyz>
2025-07-21 14:42:07 -0600

    feat: enhance FlushUsernameComponent to fetch price and execute username registration

--- src/components/SigConstructors/MNSFunctions/FlushUsernameComponent.tsx
+++ src/components/SigConstructors/MNSFunctions/FlushUsernameComponent.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { getAccount } from "@wagmi/core";
+import { getAccount, readContract } from "@wagmi/core";
 import { config } from "@/config/index";
 import { useMnsSignatureBuilder } from "@/utils/SignatureBuilder/useMnsSignatureBuilder";
 import { TitleAndLink } from "@/components/SigConstructors/InputsAndModules/TitleAndLink";
@@ -10,62 +10,115 @@ import { PrioritySelector } from "../InputsAndModules/PrioritySelector";
 import { NumberInputField } from "../InputsAndModules/NumberInputField";
 import { TextInputField } from "../InputsAndModules/TextInputField";
 import { DataDisplayWithClear } from "@/components/SigConstructors/InputsAndModules/DataDisplayWithClear";
+import {
+  FlushUsernameInputData,
+  PayInputData,
+} from "@/utils/TypeInputStructures";
+import { getAccountWithRetry } from "@/utils/getAccountWithRetry";
+import MateNameService from "@/constants/abi/MateNameService.json";
+import { contractAddress, tokenAddress } from "@/constants/address";
+import { executeFlushUsername } from "@/utils/TransactionExecuter";
 
-
-type FlushUsernameData = {
-  user: `0x${string}`;
-  nonce: string;
-  username: string;
-  priorityFeeForFisher: string;
-  signature: string;
-  nonce_Evvm: string;
-  priority_Evvm: string;
-  signature_Evvm: string;
+type InfoData = {
+  PayInputData: PayInputData;
+  FlushUsernameInputData: FlushUsernameInputData;
 };
 
 export const FlushUsernameComponent = () => {
   const { signFlushUsername } = useMnsSignatureBuilder();
   const account = getAccount(config);
   const [priority, setPriority] = React.useState("low");
-  const [dataToGet, setDataToGet] = React.useState<FlushUsernameData | null>(
-    null
-  );
+  const [dataToGet, setDataToGet] = React.useState<InfoData | null>(null);
+
+  const getValue = (id: string) =>
+    (document.getElementById(id) as HTMLInputElement).value;
 
   const makeSig = async () => {
-    const getValue = (id: string) =>
-      (document.getElementById(id) as HTMLInputElement).value;
+    const walletData = await getAccountWithRetry(config);
+    if (!walletData) return;
 
-    const addressMNS = getValue("mnsAddressInput_flushUsername");
-    const nonceMNS = getValue("nonceMNSInput_flushUsername");
-    const username = getValue("usernameInput_flushUsername");
-    const priceToFlushUsername = getValue("priceForFlushInput_flushUsername");
-    const priorityFeeForFisher = getValue("priorityFeeInput_flushUsername");
-    const nonceEVVM = getValue("nonceEVVMInput_flushUsername");
-    const priorityFlag = priority === "high";
+    const formData = {
+      addressMNS: getValue("mnsAddressInput_flushUsername"),
+      nonceMNS: getValue("nonceMNSInput_flushUsername"),
+      username: getValue("usernameInput_flushUsername"),
+      priorityFeeForFisher: getValue("priorityFeeInput_flushUsername"),
+      nonceEVVM: getValue("nonceEVVMInput_flushUsername"),
+      priorityFlag: priority === "high",
+    };
 
-    signFlushUsername(
-      addressMNS,
-      BigInt(nonceMNS),
-      username,
-      BigInt(priceToFlushUsername),
-      BigInt(priorityFeeForFisher),
-      BigInt(nonceEVVM),
-      priorityFlag,
-      (paySignature, flushUsernameSignature) => {
-        setDataToGet({
-          user: (account.address || "0x0") as `0x${string}`,
-          nonce: nonceMNS,
-          username: username,
-          priorityFeeForFisher: priorityFeeForFisher,
-          signature: paySignature,
-          nonce_Evvm: nonceEVVM,
-          priority_Evvm: priorityFlag.toString(),
-          signature_Evvm: flushUsernameSignature,
-        });
-      },
-      (error) => console.error("Error signing payment:", error)
-    );
+    readContract(config, {
+      abi: MateNameService.abi,
+      address: formData.addressMNS as `0x${string}`,
+      functionName: "getPriceToFlushUsername",
+      args: [formData.username],
+    })
+      .then((priceToFlushUsername) => {
+        console.log("Price to flush username:", priceToFlushUsername);
+        if (!priceToFlushUsername) {
+          console.error("Price to remove custom metadata is not available");
+          return;
+        }
+        signFlushUsername(
+          formData.addressMNS,
+          BigInt(formData.nonceMNS),
+          formData.username,
+          priceToFlushUsername as bigint,
+          BigInt(formData.priorityFeeForFisher),
+          BigInt(formData.nonceEVVM),
+          formData.priorityFlag,
+          (paySignature, flushUsernameSignature) => {
+            setDataToGet({
+              PayInputData: {
+                from: walletData.address as `0x${string}`,
+                to_address: formData.addressMNS as `0x${string}`,
+                to_identity: "",
+                token: tokenAddress.mate as `0x${string}`,
+                amount: priceToFlushUsername as bigint,
+                priorityFee: BigInt(formData.priorityFeeForFisher),
+                nonce: BigInt(formData.nonceEVVM),
+                priority: formData.priorityFlag,
+                executor: formData.addressMNS as `0x${string}`,
+                signature: paySignature,
+              },
+              FlushUsernameInputData: {
+                user: walletData.address as `0x${string}`,
+                identity: formData.username,
+                priorityFeeForFisher: BigInt(formData.priorityFeeForFisher),
+                nonce: BigInt(formData.nonceMNS),
+                signature: flushUsernameSignature,
+                nonce_Evvm: BigInt(formData.nonceEVVM),
+                priority_Evvm: formData.priorityFlag,
+                signature_Evvm: paySignature,
+              },
+            });
+          },
+          (error) => console.error("Error signing payment:", error)
+        );
+      })
+      .catch((error) => {
+        console.error("Error reading mate reward amount:", error);
+        return;
+      });
   };
+
+  const execute = async () => {
+      if (!dataToGet) {
+        console.error("No data to execute payment");
+        return;
+      }
+      const mnsAddress = dataToGet.PayInputData.to_address;
+  
+      executeFlushUsername(
+        dataToGet.FlushUsernameInputData,
+        mnsAddress
+      )
+        .then(() => {
+          console.log("Registration username executed successfully");
+        })
+        .catch((error) => {
+          console.error("Error executing registration username:", error);
+        });
+    };
 
   return (
     <div className="flex flex-1 flex-col justify-center items-center">
@@ -80,6 +133,10 @@ export const FlushUsernameComponent = () => {
         label="MNS Address"
         inputId="mnsAddressInput_flushUsername"
         placeholder="Enter MNS address"
+        defaultValue={
+          contractAddress[account.chain?.id as keyof typeof contractAddress]
+            ?.mns || ""
+        }
       />
 
       {/* Nonce section with automatic generator */}
@@ -94,12 +151,6 @@ export const FlushUsernameComponent = () => {
         label="Identity"
         inputId="usernameInput_flushUsername"
         placeholder="Enter username"
-      />
-
-      <NumberInputField
-        label="Price for flush"
-        inputId="priceForFlushInput_flushUsername"
-        placeholder="Enter price for flush"
       />
 
       <NumberInputField
@@ -131,6 +182,7 @@ export const FlushUsernameComponent = () => {
       <DataDisplayWithClear
         dataToGet={dataToGet}
         onClear={() => setDataToGet(null)}
+        onExecute={execute}
       />
     </div>
   );

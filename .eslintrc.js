6312dca746f6ab87c586e07d773779a2be9e61d0
jistro <contact@jistro.xyz>
2025-10-10 15:24:39 -0600

    feat: create ESLint configuration file with parser options

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2022, // La versión de JavaScript que desea usar
+    sourceType: "module", // Permite el uso de "import" e "export"
+  },
+};

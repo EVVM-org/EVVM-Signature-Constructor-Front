a08a7be63bc7d150645c41d168652ec1a4984422
jistro <contact@jistro.xyz>
2025-10-10 15:41:55 -0600

    fix: correct ESLint rules formatting and ensure proper newline at end of file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,14 @@
 module.exports = {
+  rules: {
+    "no-unused-vars": "off",
+  },
   parserOptions: {
     ecmaVersion: 2022, // La versión de JavaScript que desea usar
     sourceType: "module", // Permite el uso de "import" e "export"
-    
   },
   extends: [
     "eslint:recommended", // Reglas recomendadas por ESLint
     "plugin:@typescript-eslint/recommended", // Reglas recomendadas para TypeScript
-    'plugin:@next/next/recommended',
+    "plugin:@next/next/recommended",
   ],
-
 };

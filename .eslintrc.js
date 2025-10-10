147ddec1cc0673cedbf0df4342bd9edfa4b2d99b
jistro <contact@jistro.xyz>
2025-10-10 15:32:51 -0600

    feat: update ESLint configuration and add Next.js ESLint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,13 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2022, // La versión de JavaScript que desea usar
     sourceType: "module", // Permite el uso de "import" e "export"
+    
   },
+  extends: [
+    "eslint:recommended", // Reglas recomendadas por ESLint
+    "plugin:@typescript-eslint/recommended", // Reglas recomendadas para TypeScript
+    "prettier", // Asegura que Prettier y ESLint no entren en conflicto
+    'plugin:@next/next/recommended',
+  ],
+
 };
